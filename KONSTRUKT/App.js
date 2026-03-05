0304e8216252b06edd7c80eb1e5415ddb9aa7323
Iness El Atouani <iness.el-atouani@epitech.eu>
2026-03-05 15:24:53 +0100

    feat: implement navigation

--- KONSTRUKT/App.js
+++ KONSTRUKT/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import LoginPage from './src/Auth/Login/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginPage from './src/Auth/Login/Login'; 
+import RegisterPage from './src/Auth/Register/Register';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <LoginPage />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Register" component={RegisterPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
