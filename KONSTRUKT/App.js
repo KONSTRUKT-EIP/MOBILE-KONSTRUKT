28aa03ac3caae81f623dba66931f2fcf68f77a9b
Iness El Atouani <iness.el-atouani@epitech.eu>
2026-03-06 13:48:15 +0100

    feat: update

--- KONSTRUKT/App.js
+++ KONSTRUKT/App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginPage from './src/Auth/Login/Login'; 
 import RegisterPage from './src/Auth/Register/Register';
+import SuccessPage from './src/Auth/Success/Success';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +17,7 @@ export default function App() {
       >
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Register" component={RegisterPage} />
+        <Stack.Screen name="Success" component={SuccessPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
