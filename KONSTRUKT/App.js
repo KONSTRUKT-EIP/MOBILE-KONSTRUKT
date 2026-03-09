239d2d41921b23dcaf01deb4c220bb0ea2c34dac
Iness El Atouani <iness.el-atouani@epitech.eu>
2026-03-09 10:21:51 +0100

    delete: removed the comment

--- KONSTRUKT/App.js
+++ KONSTRUKT/App.js
@@ -1,33 +1,3 @@
-/*import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import LoginPage from './src/Auth/Login/Login'; 
-import RegisterPage from './src/Auth/Register/Register';
-import SuccessPage from './src/Auth/Success/Success';
-import HomePage from './src/HomePage/HomePage';
-import DashboardPage from './src/Dashboard/Dashboard';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Register" component={RegisterPage} />
-        <Stack.Screen name="Success" component={SuccessPage} />
-        <Stack.Screen name="HomePage" component={HomePage} options={{ headerShown: false }} />
-        <Stack.Screen name="Dashboard" component={DashboardPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-*/
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
